4cbeda2c150deb58d8f54d12346a8b9fd8a78ad3
Brandon Eugene Fields <brandonefields@gmail.com>
2022-09-20 13:01:47 -0600

    swal update

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 console.log("Hello, world!");
+
 $('.logo').on( "click", function(){
     Swal.fire(
         'What is this?',
         'This is my logo :)',
         'question'
       )
+})
+
+
+$('.click-to-learn-more').on( "click", function(){
+  Swal.fire(
+      'More about me!',
+      'Thank you, for your interest! I will eventually add more content!!',
+      'info'
+    )
 })
